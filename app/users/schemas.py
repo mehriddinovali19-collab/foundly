dd8b8961e75c1e4d6354d2ecb3f2f1c9d2a3e8d1
ali mekhridinov <mehriddinovali19@gmail.com>
2026-08-24 18:20:58 +0500

    feat: schemas added

---
+++ app/users/schemas.py
@@ -0,0 +1,32 @@
+from datetime import datetime 
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8)
+    confirm_password : str = Field(min_length=8)
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+
+class Config:
+    from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class TokenData(BaseModel):
+    user_id: Optional[int] = None
